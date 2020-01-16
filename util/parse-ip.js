a542d94e8df60d17d896b33accb08b8cb6dd9864
SukkaW <isukkaw@gmail.com>
2020-01-16 12:43:05 +0800

    refactor: split parseIp from parse

---
+++ util/parse-ip.js
@@ -0,0 +1,20 @@
+const { gray } = require('chalk');
+const { spacing } = require('pangu');
+const { join } = require('path');
+const QQWry = require('lib-qqwry');
+const { searchIP } = new QQWry(true, join(__dirname, '../data/qqwry.dat'));
+const { v4: isIpv4 } = require('is-ip');
+const reSimpleIP = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/gm
+
+const parseIp = str => {
+  return str.replace(reSimpleIP, match => {
+    if (isIpv4(match)) {
+      const { Country, Area } = searchIP(match);
+      return match + ' ' + gray(`[${spacing((Country + ' ' + Area).replace(/CZ88.NET/g, '').trim())}]`);
+    }
+
+    return match;
+  })
+}
+
+exports.parseIp = parseIp;
