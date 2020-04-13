a829fb0a8d16add816105216f2f204395b0fc770
SukkaW <isukkaw@gmail.com>
2020-04-13 09:56:31 +0800

    feat/chore: remove is-ip deps

--- util/parse-ip.js
+++ util/parse-ip.js
@@ -3,17 +3,13 @@ const { spacing } = require('pangu');
 const QQWry = require('lib-qqwry');
 const { qqwryDataPath } = require('./const');
 const { searchIP } = new QQWry(true, qqwryDataPath);
-const { v4: isIpv4 } = require('is-ip');
-const reSimpleIP = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/gm
+
+const ipv4Regex = /(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}/gm;
 
 const parseIp = str => {
-  return str.replace(reSimpleIP, match => {
-    if (isIpv4(match)) {
+  return str.replace(ipv4Regex, match => {
       const { Country, Area } = searchIP(match);
       return match + ' ' + gray(`[${spacing((Country + ' ' + Area).replace(/CZ88.NET/g, '').trim())}]`);
-    }
-
-    return match;
   })
 }
 
