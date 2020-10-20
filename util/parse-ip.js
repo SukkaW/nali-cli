9b8ef5110effbd3fbd9a73980b22f0c0886088ee
Outvi V <git@outv.im>
2020-10-20 09:46:31 +0800

    fix: avoid matching IPs in parts of URL

--- util/parse-ip.js
+++ util/parse-ip.js
@@ -4,7 +4,7 @@ const QQWry = require('lib-qqwry');
 const { qqwryDataPath } = require('./const');
 const { searchIP } = new QQWry(true, qqwryDataPath);
 
-const ipv4Regex = /(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}/gm;
+const ipv4Regex = /(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}(?=[\s$])/gm;
 
 const parseIp = str => {
   return str.replace(ipv4Regex, match => {
