bc7db52c14df261c3b44b91c72ecf64ec516608b
Lee <44310445+lx200916@users.noreply.github.com>
2021-12-08 20:28:18 +0800

    1.Execute `tracert -d` instead of `traceroute` on Win32. 2.Error Tips on Win32.

--- util/execute.js
+++ util/execute.js
@@ -1,4 +1,5 @@
 const { spawn } = require('child_process');
+const os = require('os')
 
 function main(exe, args) {
   require('./check-dat')(() => {
@@ -26,7 +27,12 @@ function main(exe, args) {
 
     command.on('error', err => {
       if (err.code === 'ENOENT') {
-        console.error(`ENOENT: Nali CLI 找不到 ${exe}, 请先安装或检查 PATH!`);
+
+          console.error(`ENOENT: Nali CLI 找不到 ${exe}, 请先安装或检查 PATH!`);
+        if (os.platform()=='win32') {
+          console.info(`Win32: Windows系统不包含dig/tracepath等命令,请自行安装!`);
+
+        }
       } else {
         console.error('嗷呜，出现错误惹! ' + err.message || err);
       }
