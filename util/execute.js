438c21111bf5a64af3de7ccd04fa2dbfda3867aa
yiqiao <yiqiao@qunhemail.com>
2023-09-01 18:37:40 +0800

    feat: support remote parse ip info online

--- util/execute.js
+++ util/execute.js
@@ -1,37 +1,35 @@
-const { spawn } = require('child_process');
+const { spawn } = require("child_process");
 
 function main(exe, args) {
-  require('./check-dat')(() => {
-    const parseIp = require('../util/parse-ip');
-    const parseCdn = require('../util/parse-cdn');
-    const command = spawn(exe, args);
+  const parseIp = require("../util/parse-ip");
+  const parseCdn = require("../util/parse-cdn");
+  const command = spawn(exe, args);
 
-    if (exe === 'dig' || exe === 'nslookup') {
-      command.stdout.on('data', data => {
-        process.stdout.write(parseIp(parseCdn(data.toString())));
-      });
+  if (exe === "dig" || exe === "nslookup") {
+    command.stdout.on("data", async (data) => {
+      process.stdout.write(await parseIp(parseCdn(data.toString())));
+    });
+  } else {
+    command.stdout.on("data", async (data) => {
+      process.stdout.write(await parseIp(data.toString()));
+    });
+  }
+
+  command.stderr.on("data", (data) => {
+    process.stdout.write(data.toString());
+  });
+
+  // command.on('exit', code => {
+  //   process.exit(code);
+  // });
+
+  command.on("error", (err) => {
+    if (err.code === "ENOENT") {
+      console.error(`ENOENT: Nali CLI 找不到 ${exe}, 请先安装或检查 PATH!`);
     } else {
-      command.stdout.on('data', data => {
-        process.stdout.write(parseIp(data.toString()));
-      });
+      console.error("嗷呜，出现错误惹! " + err.message || err);
     }
-
-    command.stderr.on('data', data => {
-      process.stdout.write(data.toString());
-    });
-
-    command.on('exit', code => {
-      process.exit(code);
-    });
-
-    command.on('error', err => {
-      if (err.code === 'ENOENT') {
-        console.error(`ENOENT: Nali CLI 找不到 ${exe}, 请先安装或检查 PATH!`);
-      } else {
-        console.error('嗷呜，出现错误惹! ' + err.message || err);
-      }
-    });
-  })
+  });
 }
 
 module.exports = main;
--- util/parse-ip.js
+++ util/parse-ip.js
@@ -1,16 +1,29 @@
-const { gray } = require('chalk');
-const { spacing } = require('pangu');
-const QQWry = require('lib-qqwry');
-const { qqwryDataPath } = require('./const');
-const { searchIP } = new QQWry(true, qqwryDataPath);
+const { gray } = require("chalk");
+const { spacing } = require("pangu");
+const replaceAsync = require("string-replace-async");
+const { get } = require("axios");
 
-const ipv4Regex = /(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}/gm;
+const ipv4Regex =
+  /(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}/gm;
 
-const parseIp = str => {
-  return str.replace(ipv4Regex, match => {
-      const { Country, Area } = searchIP(match);
-      return match + ' ' + gray(`[${spacing((Country + ' ' + Area).replace(/CZ88.NET/g, '').trim())}]`);
-  })
+async function getIpInfo(ip) {
+  return new Promise((resolve, reject) => {
+    get(`http://tbip.alicdn.com/api/getipinfo?ip=${ip}`).then(({ data }) => {
+      const { country, region, city, isp } = data.data;
+      const ipInfo = [country, region, city, isp]
+        .filter((v) => v !== "XX")
+        .join(" ")
+        .trim();
+      resolve(ipInfo);
+    });
+  });
 }
 
-module.exports = parseIp;
+async function parseIpAsync(str) {
+  return await replaceAsync(str, ipv4Regex, async (match) => {
+    const ipInfo = await getIpInfo(match);
+    return match + " " + gray(`[${spacing(ipInfo)}]`);
+  });
+}
+
+module.exports = parseIpAsync;
