04f3a7c2613b50365353866f7a01bdf1f9cd2d7e
fantasyroot <anto17@foxmail.com>
2023-09-08 18:23:09 +0800

    feat: query info from one of server list

--- util/const.js
+++ util/const.js
@@ -1,4 +1,63 @@
-const { join } = require('path');
-const { homedir } = require('os');
+const QUERY_IP_SERVER_MAP = {
+  taobao: {
+    getUrl: (ip) => `http://tbip.alicdn.com/api/getipinfo?ip=${ip}`,
+    country: ["data", "country"],
+    region: ["data", "region"],
+    city: ["data", "city"],
+    isp: ["data", "isp"],
+  },
+  "ipinfo.io": {
+    getUrl: (ip) => `https://ipinfo.io/${ip}/json`,
+    country: "country",
+    region: "region",
+    city: "city",
+    isp: "org",
+  },
+  "ip.sb": {
+    getUrl: (ip) => `https://api.ip.sb/geoip/${ip}`,
+    country: "country",
+    region: "region",
+    city: "city",
+    isp: "isp",
+  },
+  "ip-api": {
+    getUrl: (ip) =>
+      `https://pro.ip-api.com/json/${ip}?fields=16985625&key=EEKS6bLi6D91G1p`,
+    country: "country",
+    region: "regionName",
+    city: "city",
+    isp: "isp",
+  },
+  ipstack: {
+    getUrl: (ip) =>
+      `https://api.ipstack.com/${ip}?access_key=e53fd9c4e2f6f73161821bb7b0df0069`,
+    country: "country_name",
+    region: "region_name",
+    city: "city",
+    isp: ["connection", "isp"],
+  },
+  ipdata: {
+    getUrl: (ip) =>
+      `https://api.ipdata.co/${ip}?api-key=513d4b07583037a5a89b6cff4ebff0083bef180977dc71dd73804cf8`,
+    country: "country_name",
+    region: "region",
+    city: "city",
+    isp: ["asn", "name"],
+  },
+  ipwho: {
+    getUrl: (ip) => `https://ipwho.is/${ip}`,
+    country: "country",
+    region: "region",
+    city: "city",
+    isp: ["connection", "isp"],
+  },
+  ipregistry: {
+    getUrl: (ip) => `https://api.ipregistry.co/${ip}?key=pm3hsjigbozt9shn`,
+    country: ["location", "country", "name"],
+    region: ["location", "region", "name"],
+    city: ["location", "city"],
+    isp: ["connection", "organization"],
+  },
+};
 
-module.exports.qqwryDataPath = join(homedir(), '.config/nali-cli/qqwry.dat');
+module.exports.QUERY_IP_SERVER_MAP = QUERY_IP_SERVER_MAP;
--- util/parse-ip.js
+++ util/parse-ip.js
@@ -1,28 +1,70 @@
 const { gray } = require("chalk");
-const { spacing } = require("pangu");
+const pangu = require("pangu");
 const replaceAsync = require("string-replace-async");
 const { get } = require("axios");
+const { QUERY_IP_SERVER_MAP } = require("./const");
+
+const QUERY_SERVER_NAME = "taobao";
 
 const ipv4Regex =
   /(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}/gm;
 
+const getValueByKey = (key, dataObj, schema) => {
+  const keyOfSchema = schema[key];
+  if (Array.isArray(keyOfSchema)) {
+    return schema[key].reduce((tempResult, currentKey) => {
+      if (!tempResult.hasOwnProperty(currentKey)) {
+        return undefined;
+      }
+      return tempResult[currentKey];
+    }, dataObj);
+  }
+
+  if (!dataObj.hasOwnProperty(keyOfSchema)) {
+    return undefined;
+  }
+
+  return dataObj[keyOfSchema];
+};
+
+const getFormattedIpInfo = (res, serverSchema) => {
+  return {
+    country: getValueByKey("country", res, serverSchema),
+    region: getValueByKey("region", res, serverSchema),
+    city: getValueByKey("city", res, serverSchema),
+    isp: getValueByKey("isp", res, serverSchema),
+  };
+};
+
 async function getIpInfo(ip) {
-  return new Promise((resolve, reject) => {
-    get(`http://tbip.alicdn.com/api/getipinfo?ip=${ip}`).then(({ data }) => {
-      const { country, region, city, isp } = data.data;
-      const ipInfo = [country, region, city, isp]
-        .filter((v) => v !== "XX")
-        .join(" ")
-        .trim();
-      resolve(ipInfo);
-    });
+  return new Promise((resolve) => {
+    const serverSchema = QUERY_IP_SERVER_MAP[QUERY_SERVER_NAME];
+    get(serverSchema.getUrl(ip), { timeout: 3000 })
+      .then(({ data }) => {
+        const { country, region, city, isp } = getFormattedIpInfo(
+          data,
+          serverSchema
+        );
+        const ipInfo = [...new Set([country, region, city, isp])]
+          .filter((v) => !!v && v !== "XX")
+          .join(", ")
+          .trim();
+        resolve(ipInfo);
+      })
+      .catch(() => {
+        resolve();
+      });
   });
 }
 
 async function parseIpAsync(str) {
   return await replaceAsync(str, ipv4Regex, async (match) => {
     const ipInfo = await getIpInfo(match);
-    return match + " " + gray(`[${spacing(ipInfo)}]`);
+    if (ipInfo) {
+      return match + " " + gray(`[${pangu.spacing(ipInfo)}]`);
+    }
+
+    return match;
   });
 }
 
